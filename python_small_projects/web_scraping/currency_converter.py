4190f98d5cd6c6cfb9b14a32c423d5a9bac01f90
RupKaur <53045118+RupKaur2001@users.noreply.github.com>
2019-07-26 18:12:00 +0530

    Added a function to truncate output using math class

--- python_small_projects/web_scraping/currency_converter.py
+++ python_small_projects/web_scraping/currency_converter.py
@@ -1,9 +1,13 @@
 #this will use an online API for scraping data. I will eventually learn how to scrape data on my own
 import requests
+#to truncate the decimal output to four decimal places
+import math 
 #requests module needed for https web requests
 #i will be using Indian Rupee(INR) as a base currency
 url = 'https://api.exchangerate-api.com/v4/latest/INR'
-
+def truncate_dec(amount):
+    #we are truncating the float value to 4 decimal places
+    return math.trunc(amount*(10**4))/(10**4)
 response=requests.get(url)
 #get the response from the url
 data=response.json()
@@ -18,6 +22,6 @@ print(data['rates'].keys())
 start=input("Which currency do you have currently?")
 end=input("Which currency do you want?")
 #I HAVE TO ADD A PART WHERE we check whether these are actual currencies present
-start_amt=int(input('Input the amount of money you have '))
+start_amt=float(input('Input the amount of money you have '))
 end_amt=start_amt*float(data['rates'][end])/float(data['rates'][start])
-print(end_amt)
+print(truncate_dec(end_amt))
