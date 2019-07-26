3055e5b20a4184d9bfc82b76ea6dd5dfffa5a050
RupKaur <53045118+RupKaur2001@users.noreply.github.com>
2019-07-26 17:59:46 +0530

    Initial Commit

---
+++ python_small_projects/web_scraping/currency_converter.py
@@ -0,0 +1,23 @@
+#this will use an online API for scraping data. I will eventually learn how to scrape data on my own
+import requests
+#requests module needed for https web requests
+#i will be using Indian Rupee(INR) as a base currency
+url = 'https://api.exchangerate-api.com/v4/latest/INR'
+
+response=requests.get(url)
+#get the response from the url
+data=response.json()
+#that response will be a json. JSON is basically a great way to get data because computers understand it easily
+''' this code shows you that the data object that we get will be in the form of a dictionary
+print(type(data))
+print(data)
+'''
+print("Enter Currency code like INR, USD etc")
+print('We offer conversion between the following currencies')
+print(data['rates'].keys())
+start=input("Which currency do you have currently?")
+end=input("Which currency do you want?")
+#I HAVE TO ADD A PART WHERE we check whether these are actual currencies present
+start_amt=int(input('Input the amount of money you have '))
+end_amt=start_amt*float(data['rates'][end])/float(data['rates'][start])
+print(end_amt)
